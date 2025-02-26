8e60001c7c88a50170008530a4867a62450815c0
Alice <alicelopess.art@gmail,com>
2025-02-26 19:22:30 +0000

    Exercicio 4

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Insere um número inteiro não negativo: ");
+            while (!int.TryParse(Console.ReadLine(), out numero) || numero < 0 || byte.MaxValue)
+            Console.WriteLine(numero / 2);
+            Console.WriteLine(numero << 3);
+            Console.WriteLine(numero ^ 6);
+            Console.WriteLine(numero > 10);
+        }
+    }
+}
