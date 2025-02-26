fd43d3108e596e56b074c6bdbd0947fca594c020
Alice <alicelopess.art@gmail,com>
2025-02-26 18:52:37 +0000

    exercicio 1

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Insere um número inteiro: ");
+            while (!int.TryParse(Console.ReadLine(), out numero) || numero < sbyte.MinValue || numero > sbyte.MaxValue)
+            sbyte n = (sbyte)numero;
+            Console.WriteLine(--n);
+            Console.WriteLine(++n);
+            
+        }
+    }
+}
+
