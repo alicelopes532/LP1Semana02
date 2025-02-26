3f6ea478d4fdb93b5977d3254bf99360700fdbc4
Alice <alicelopess.art@gmail,com>
2025-02-26 19:42:27 +0000

    Exercicio 5

---
+++ Cilindro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            double altura, raio;
+            Console.WriteLine("Insere a altura do cilindro: ");
+            while (!double.TryParse(Console.ReadLine(), NumberStyles.Float, out altura))
+            Console.WriteLine("Insere o raio do cilindro: ");
+            while (!double.TryParse(Console.ReadLine(), NumberStyles.Float, out raio))
+            double volume = Math.PI * Math.Pow(raio, 2) * altura;
+            double areaSuperficie = 2 * Math.PI * raio (raio + altura);
+            Console.WriteLine($"Volume = {volume:F3}");
+            Console.WriteLine($"Área de superfície = {areaSuperficie:F3}");
+        }
+    }
+}
