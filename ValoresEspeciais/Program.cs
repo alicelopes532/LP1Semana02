e94750451fd013c1dee2fc3c8d91b723371ee275
Alice <alicelopess.art@gmail,com>
2025-02-26 19:55:47 +0000

    Imprimir valores máximos e mínimos de todos os tipos conhecidos

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Valores Mínimos e Máximos de Tipos Numéricos:");
+            Console.WriteLine($"sbyte:   {sbyte.MinValue} a {sbyte.MaxValue}");
+            Console.WriteLine($"byte:    {byte.MinValue} a {byte.MaxValue}");
+            Console.WriteLine($"short:   {short.MinValue} a {short.MaxValue}");
+            Console.WriteLine($"ushort:  {ushort.MinValue} a {ushort.MaxValue}");
+            Console.WriteLine($"int:     {int.MinValue} a {int.MaxValue}");
+            Console.WriteLine($"uint:    {uint.MinValue} a {uint.MaxValue}");
+            Console.WriteLine($"long:    {long.MinValue} a {long.MaxValue}");
+            Console.WriteLine($"ulong:   {ulong.MinValue} a {ulong.MaxValue}");
+            Console.WriteLine($"float:   {float.MinValue} a {float.MaxValue}");
+            Console.WriteLine($"double:  {double.MinValue} a {double.MaxValue}");
+            Console.WriteLine($"decimal: {decimal.MinValue} a {decimal.MaxValue}");
+        }
+    }
+}
